5abcc4702757568b9f4bd20dfa41ba73e583a63f
KG <khatunaggg12345@gmail.com>
2024-06-14 22:35:57 +0400

    correction

--- src/app/components/Login/Login.tsx
+++ src/app/components/Login/Login.tsx
@@ -86,7 +86,7 @@ const Login = () => {
       <div className="w-full flex flex-row items-center justify-start gap-2 text-xs lg:text-sm">
         <input type="checkbox" name="" id="" />
         <label htmlFor="">
-          Do not have an account?{" "}
+          Do not have an account?
           <span className="font-bold text-green-950"> Sign up </span>
         </label>
       </div>
